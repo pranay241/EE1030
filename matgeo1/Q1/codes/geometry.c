80191c3678c847952d71254a6b527ffdb1c5142f
pranay241 <ee24btech11011@iith.ac.in>
2024-09-15 17:04:31 +0530

    Delete matgeo1/Q1 directory

--- matgeo1/Q1/codes/geometry.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void section(double x1, double y1, double x2, double y2, int k, double *x_div, double *y_div) {
-    *x_div = (k * x2 + x1) / (k + 1);
-    *y_div = (k * y2 + y1) / (k + 1);
-}
-
